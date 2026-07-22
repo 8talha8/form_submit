c869fa56d1d802c67b74d7a23b50716d1f7b3cc3
Mohammed Talha <8talha8@gmail.com>
2026-07-22 22:13:31 +0530

    second

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // GRADLE PLUGINS
 plugins {
-    java
-    id("org.springframework.boot") version property("springBootPluginVersion")
-    id("io.spring.dependency-management") version property("dependencyManagementPluginVersion")
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "1.9.24" // if using Kotlin
 }
 
 repositories {
