cde08563218b4c60e28f108689bc1ce25c5c1d66
abuzar <6abuzar9@gmail.com>
2026-07-27 00:08:57 +0530

    new csv

--- src/test/java/com/example/seleniumdemo/service/MappingServiceIntegrationTest.java
+++ src/test/java/com/example/seleniumdemo/service/MappingServiceIntegrationTest.java
@@ -4,6 +4,7 @@ import com.example.seleniumdemo.model.FieldMapping;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 import java.util.Map;
